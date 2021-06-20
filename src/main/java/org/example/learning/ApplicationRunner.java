794431b3ff67e282efe5e9001760e34acab2e15b
pm-erman <erman@policmedical.com>
2021-06-20 18:30:36 +0300

    commitledim

--- src/main/java/org/example/learning/ApplicationRunner.java
+++ src/main/java/org/example/learning/ApplicationRunner.java
@@ -7,7 +7,14 @@ public class ApplicationRunner {
 
         int[] array = new int[]{123,43,123,66,434,11,234,77,554};
 
+        boolean buldum= false;
+
+        int aranan = 11;
 
 
+        //for dongusu kurcaksınız
+
+
+        System.out.println(buldum);
     }
 }
