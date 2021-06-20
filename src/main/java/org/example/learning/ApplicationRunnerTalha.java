eca3830ac278a01de9726a2cda7f478815b0cf98
Talhaymn <talhay93@gmail.com>
2021-06-20 18:41:00 +0300

    yeni yorum eklendi

--- src/main/java/org/example/learning/ApplicationRunnerTalha.java
+++ src/main/java/org/example/learning/ApplicationRunnerTalha.java
@@ -4,8 +4,24 @@ public class ApplicationRunnerTalha {
 
 //Soru1
     public static void main(String[] args) {
-
         int[] array = new int[]{123,43,123,66,434,11,234,77,554};
+
+        boolean buldum= false;
+
+        int aranan = 11;
+
+
+        for (int i = 0 ; i <array.length ; i++){
+            if ( array[i]==aranan){
+                System.out.prinln("sayı mevcut");
+            }
+            else
+                System.out.println("sayı mevcut değil")
+        }
+
+        //for dongusu kurcaksınız
+
+
 
 
     }
