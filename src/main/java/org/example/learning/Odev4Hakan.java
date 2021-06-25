d1d526d26584d40f03e0d15141937c32440fba59
hakan <hakan-celik98@outlook.com>
2021-06-26 02:46:05 +0300

    odev4 soru eklendi

---
+++ src/main/java/org/example/learning/Odev4Hakan.java
@@ -0,0 +1,14 @@
+package org.example.learning;
+
+public class Odev4Hakan {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{123, 43, 123, 66, 434, 11, 234, 77, 554};
+
+        for(int i=8; i>=0; i--)
+        {
+            System.out.println(array[i]);
+        }
+    }
+}
